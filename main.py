b48ba41e2bfd289c8020f55a851edba714d849bd
tipx2 <57505742+tipx2@users.noreply.github.com>
2022-06-21 07:10:47 +0100

    added inefficient redline stuff

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from random import shuffle
 
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
-import moviepy.editor as mp
 from pydub import AudioSegment
 import numpy as np
 import cv2
@@ -16,12 +15,13 @@ def natural_key(string_):
     return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
 
 
-def plot_frame(xcoords, arr, loopcount, folder, greenlines, frame_rate):
+def plot_frame(xcoords, arr, loopcount, folder, greenlines, redlines, frame_rate):
     
     if len(greenlines) == 0:
         sound = 0
     else:
         sound = arr[greenlines[0]]
+        
     sps = 44100
     freq_hz = 440.0
     duration = (1/frame_rate)
@@ -39,18 +39,20 @@ def plot_frame(xcoords, arr, loopcount, folder, greenlines, frame_rate):
     for i in range(0, len(arr)):
         if i in greenlines:
             l.append((0.1,1,0.1))
+        elif i in redlines:
+            l.append((1,0.1,0.1))
         else:
-            l.append(((arr[i]/len(arr)),0,(arr[i]/len(arr))))
+            l.append((0,0.5,(arr[i]/len(arr))))
 
-    plt.figure(facecolor='#e68332')
+    plt.figure(facecolor='#a5ffd6')
     plt.bar(xcoords, arr, width=1, color=l)
     plt.axis("off")
-    plt.savefig(folder + "/frame" + str(loopcount) + ".jpg")
+    plt.savefig(folder + "/frame" + str(loopcount) + ".png")
     plt.close()
 
 
 def render_video(video_name, image_folder, frame_rate):
-    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
     images = sorted(images, key=natural_key)
     images = images + [images[-1] for x in range(20)]
     frame = cv2.imread(os.path.join(image_folder, images[0]))
@@ -73,11 +75,6 @@ def render_audio(output, folder, video):
     for sound in sounds:
         bigsound += AudioSegment.from_wav(folder + "/" + sound)
     bigsound.export(output, format="wav")
-    
-    audio = mp.AudioFileClip(output)
-    video1 = mp.VideoFileClip(video)
-    final = video1.set_audio(audio)
-    final.write_videofile(output + "_final.mp4",codec= 'mpeg4' ,audio_codec='libvorbis')
 
 
 def insertion_sort(arr, folder, frame_rate):
@@ -88,7 +85,7 @@ def insertion_sort(arr, folder, frame_rate):
 		p = i
 		while p > -1:
 			loopcount += 1
-			plot_frame(xcoords, arr, loopcount, folder, [p, p + 1], frame_rate)
+			plot_frame(xcoords, arr, loopcount, folder, [p, p + 1], [], frame_rate)
 			if arr[p+1] < arr[p]:
 				temp = arr[p]
 				arr[p] = arr[p + 1]
@@ -108,15 +105,14 @@ def bogo_sort(arr, folder, frame_rate):
         shuffle(arr)
         for x in range(len(arr)-1):
             if arr[x] > arr[x+1]:
-                plot_frame(xcoords, arr, loopcount, folder, [x, x+1], frame_rate)
+                plot_frame(xcoords, arr, loopcount, folder, [x, x+1],[], frame_rate)
                 arr_sorted = False
                 break
             else:
-                plot_frame(xcoords, arr, loopcount, folder, [], frame_rate)
+                plot_frame(xcoords, arr, loopcount, folder, [],[], frame_rate)
                 arr_sorted = True
-            
-        
-            
+
+
 
 def bubble_sort(arr, folder, frame_rate):
     os.mkdir(folder + "/")
@@ -127,15 +123,37 @@ def bubble_sort(arr, folder, frame_rate):
         swapped = False
         for x in range(len(arr)-1):
             loopcount += 1
-            plot_frame(xcoords, arr, loopcount, folder, [x+1], frame_rate)
+            plot_frame(xcoords, arr, loopcount, folder, [x+1],[], frame_rate)
             if arr[x] > arr[x+1]:
                 arr[x],arr[x+1], swapped = arr[x+1], arr[x], True
     loopcount += 1
-    plot_frame(xcoords, arr, loopcount, folder, [], frame_rate)
+    plot_frame(xcoords, arr, loopcount, folder, [],[], frame_rate)
+
+def swapPositions(lis, pos1, pos2):
+    lis[pos1], lis[pos2] = lis[pos2], lis[pos1]
 
 
-xcoords = [x for x in range(1,50)]
-arr = [x for x in range(1,50)]
+def selection_sort(arr, folder, frame_rate):
+    os.mkdir(folder + "/")
+    os.mkdir("sounds_" + folder + "/")
+    loopcount = 0
+    for x in range(len(arr)-1):
+        lowest = arr[x]
+        for y in range(len(arr[x:])):
+            loopcount += 1
+            if arr[x:][y] < lowest:
+                lowest = arr[x:][y]
+            plot_frame(xcoords, arr, loopcount, folder, [x, y], [arr.index(lowest)], frame_rate)
+            
+        
+        lowest = min(arr[x:])
+        swapPositions(arr, x, arr.index(lowest))
+        
+        
+    plot_frame(xcoords, arr, loopcount, folder, [], [], frame_rate)
+
+xcoords = [x for x in range(1,100)]
+arr = [x for x in range(1,100)]
 
 shuffle(arr)
 
@@ -143,10 +161,14 @@ shuffle(arr)
 # render_video("insertion.mp4", "insertion_frames", 30)
 # render_audio("insertion.wav", "sounds_insertion_frames", "insertion.mp4")
 
-bubble_sort(arr, "bubble_frames", 60)
-render_video("bubble1.mp4", "bubble_frames", 60)
-render_audio("bubble.wav", "sounds_bubble_frames", "bubble1.mp4")
+# bubble_sort(arr, "bubble_frames", 60)
+# render_video("bubble1.mp4", "bubble_frames", 60)
+# render_audio("bubble.wav", "sounds_bubble_frames", "bubble1.mp4")
 
-# bogo_sort(arr, "bogo_frames", 60)
-# render_video("bogo.mp4", "bogo_frames", 60)
+# bogo_sort(arr, "bogo_frames", 15)
+# render_video("bogo.mp4", "bogo_frames", 15)
 # render_audio("bogo.wav", "sounds_bogo_frames", "bogo.mp4")
+
+selection_sort(arr, "selection_frames", 15)
+render_video("selection.mp4", "selection_frames", 15)
+render_audio("selection.wav", "sounds_selection_frames", "selection.mp4")
